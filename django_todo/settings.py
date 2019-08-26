678f4a1f458c2dc98f14ddda9f8d24da2eabb296
Ubuntu <ubuntu@ip-172-31-28-88.eu-west-1.compute.internal>
2019-08-26 09:49:25 +0000

    Updated environment variables in settings so that would run on Heroku and C9

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dx3vj_pf4(f&k@n6ra6*9l+pbehqef2wlf9_505wh0$w3+q6(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 
                     os.environ.get('HOSTNAME')]
@@ -76,13 +82,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+if development:
+    DATABASES = {
+       'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3')}}
+else: 
+    DATABASES = {"default": dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
-#DATABASES = {
-#   'default': {
-#   'ENGINE': 'django.db.backends.sqlite3',
-#   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#}
-#}
 
 DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
